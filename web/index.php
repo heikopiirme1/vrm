649978e53743e6384753a7c198a13a9f88eac4f8
“Heiko <heiko.piirme@khk.ee>
2018-09-10 15:02:21 +0300

    Added Twig to the project, first template for /booking/create route.

--- web/index.php
+++ web/index.php
@@ -4,8 +4,18 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), [
+	'twig.path' => __DIR__.'/../views',
+]);
+
 $app->get('/hello', function () {
     return 'Hello world!';
+});
+
+$app->get('/bookings/create', function () use ($app) {
+	return $app['twig']->render('base.html.twig');
 });
 
 $app->run();
