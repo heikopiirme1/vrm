f898f7a4e90f71258da4278d7d3860f74898b15c
“Heiko <heiko.piirme@khk.ee>
2018-09-13 09:47:24 +0300

    Created and connected database using Doctrine and SQLite

--- web/index.php
+++ web/index.php
@@ -6,9 +6,37 @@ $app = new Silex\Application();
 
 $app['debug'] = true;
 
+
+
 $app->register(new Silex\Provider\TwigServiceProvider(), [
 	'twig.path' => __DIR__.'/../views',
 ]);
+
+//Andmebaasi konfiguratsioon
+$app->register(new Silex\Provider\DoctrineServiceProvider(), [
+	'db.options' => [
+		'driver' => 'pdo_sqlite',
+		'path' => __DIR__.'/../database/app.db',
+	],
+]);
+
+//Tehakse tabel 'bookings', kui seda veel olemas ei ole.
+if (!$app['db']->getSchemaManager()->tablesExist('bookings')){
+	$app['db']->executeQuery("CREATE TABLE bookings (
+		id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		firstName VARCHAR(40) NOT NULL,
+		lastName VARCHAR(40) NOT NULL,
+		phone VARCHAR(10) NOT NULL,
+		email VARCHAR(20) DEFAULT NULL,
+		birthday DATE NOT NULL,
+		startDate DATE NOT NULL,
+		endDate DATE NOT NULL,
+		arrivalTime TIME DEFAULT NULL,
+		additionalInformation TEXT,
+		nrOfPeople INT NOT NULL,
+		payingMethod VARCHAR(10) NOT NULL
+		);");
+}
 
 $app->get('/hello', function () {
     return 'Hello world!';
